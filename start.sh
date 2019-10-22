6586ec09df2ad05dfe610e4acf9d02fc6e9641d9
Alan Steremberg <alan.steremberg@gmail.com>
2019-10-22 13:45:44 -0700

    fixes

--- start.sh
+++ start.sh
@@ -11,3 +11,8 @@ sudo apt-get update && apt-get install -y \
     cd /usr/local/src/flatzebra-0.1.6/ &&\
     ./configure && make && make install &&\
     ldconfig
+echo "add display_rotate=3 to /boot/config.txt"
+echo "turn on i2c"
+echo "dtparam=i2c_arm=on"
+echo "sudo vi /etc/xdg/lxsession/LXDE-pi/autostart"
+echo "add @/home/pi/burgerspace/run"
