8f89757edfb563059b7510f5d36c501981e6a831
Alan Steremberg <alan.steremberg@gmail.com>
2019-10-22 13:42:47 -0700

    added start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+sudo apt-get update && apt-get install -y \
+    build-essential \
+    libsdl-image1.2-dev \
+    libsdl-mixer1.2-dev \
+    pkg-config \
+    wget &&\
+    mkdir -p /usr/local/src &&\
+    cd /usr/local/src &&\
+    wget https://perso.b2b2c.ca/~sarrazip/dev/flatzebra-0.1.6.tar.gz && tar zxf flatzebra-0.1.6.tar.gz && \
+    rm flatzebra-0.1.6.tar.gz && \
+    cd /usr/local/src/flatzebra-0.1.6/ &&\
+    ./configure && make && make install &&\
+    ldconfig
