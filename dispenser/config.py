7976c81e448588ba8144cc98c05d959930aa8ff5
Alan Steremberg <alan.steremberg@gmail.com>
2019-10-23 14:20:15 -0700

    fixed values

--- dispenser/config.py
+++ dispenser/config.py
@@ -1,4 +1,4 @@
-servo_max = 100  # Min pulse length out of 4096
-servo_min = 630  # Max pulse length out of 4096
+servo_max = 260  # Min pulse length out of 4096
+servo_min = 600  # Max pulse length out of 4096
 servo_channel = 0
 
