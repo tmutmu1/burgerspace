9a76ccad6d818d6efdf1787520fed74228a85be8
Alan Steremberg <alan.steremberg@gmail.com>
2019-10-23 14:08:07 -0700

    fixes

--- dispenser/test.py
+++ dispenser/test.py
@@ -17,12 +17,12 @@ def test():
 def main(argv, stdout, environ):
   if sys.version_info < (3, 0): reload(sys); sys.setdefaultencoding('utf8')
 
-  parser, args = parse_args(argv)
+  #parser, args = parse_args(argv)
 
-  numeric_loglevel = getattr(logging, args.log_level.upper(), None)
-  if not isinstance(numeric_loglevel, int):
-    raise ValueError('Invalid log level: %s' % args.log_level)
-
+  #numeric_loglevel = getattr(logging, args.log_level.upper(), None)
+  #if not isinstance(numeric_loglevel, int):
+  #  raise ValueError('Invalid log level: %s' % args.log_level)
+  numeric_loglevel=3
   logging.basicConfig(format="[%(asctime)s] %(levelname)-8s %(message)s",
                        datefmt="%m/%d %H:%M:%S", level=numeric_loglevel)
 
